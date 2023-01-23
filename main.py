767b85f1bde0754be3244d754619f6add8cf26ab
Adeesh M Shrestha <35621337+adeeshmans@users.noreply.github.com>
2023-01-23 03:54:02 +0000

    comments

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
 
 #convert strings into integer and float
-weight_as_int = int(weight)
-height_as_float = float(height)
+weight_new = int(weight)
+height_new = float(height)
 
 # Formula
 # Using the exponent operator **
-bmi = weight_as_int / height_as_float ** 2
-# or using multiplication and PEMDAS
-bmi = weight_as_int / (height_as_float * height_as_float)
+bmi = weight_new / height_new ** 2
 
 bmi_as_int = int(bmi)
 
